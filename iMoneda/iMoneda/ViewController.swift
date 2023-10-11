8870106bf6a201e76b16f724052f7a1150c46c23
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2023-10-11 11:41:17 +0200

    ProjectCreated

---
+++ iMoneda/iMoneda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iMoneda
+//
+//  Created by Raquel Guerrero Perucho on 11/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
