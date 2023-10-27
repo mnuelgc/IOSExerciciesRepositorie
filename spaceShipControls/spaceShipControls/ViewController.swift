72b2ebd7a30e5debbca2b508aea83e15f19c1153
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2023-10-27 18:26:53 +0200

    TextField done

--- spaceShipControls/spaceShipControls/ViewController.swift
+++ spaceShipControls/spaceShipControls/ViewController.swift
@@ -2,18 +2,38 @@
 //  ViewController.swift
 //  spaceShipControls
 //
-//  Created by Raquel Guerrero Perucho on 26/10/23.
+//  Created by Manuel Gómez Cámara on 26/10/23.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var editTextObject: UITextField!
+    
+    @IBOutlet weak var text: UITextView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func introPressed(_ sender: UITextField) {
+        
+        if let texto = editTextObject.text{
+            if (texto != ""){
+                text.text += texto + "\n"
+            }
+        }
 
+        sender.resignFirstResponder()
+
+    }
+    
+    
+    
 }
 
