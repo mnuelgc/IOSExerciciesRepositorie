41e4e266383b91e30b09892bfbc3804b7086f2ba
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2023-11-01 12:16:45 +0100

    Interfaz realizada

---
+++ Pioneras/Pioneras/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Pioneras
+//
+//  Created by Manuel Gómez Cámara on 1/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func retornoDeSecundaria(segue: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
