914ba91b6e558c6399acdac988f5d9083941ae01
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2023-11-03 16:48:08 +0100

    API setted

--- Marvel/Marvel/ViewController.swift
+++ Marvel/Marvel/ViewController.swift
@@ -13,8 +13,8 @@ class ViewController: UIViewController {
     func mostrarPersonajes(comienzanPor cadena : String) {
             let marvelAPI = RCMarvelAPI()
             //PUEDES CAMBIAR ESTO PARA PONER TUS CLAVES
-            marvelAPI.publicKey = "a6927e7e15930110aade56ef90244f6d"
-            marvelAPI.privateKey = "487b621fc3c0d6f128b468ba86c99c508f24d357"
+            marvelAPI.publicKey = "9f21850b7afb93929bb2f044d2d1f661"
+            marvelAPI.privateKey = "59c50fcb3b22a4e64ed4827aeeb949d323fe67f0"
             let filtro = RCCharacterFilter()
             filtro.nameStartsWith = cadena
             filtro.limit = 50
